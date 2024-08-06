25a1b7c43e8ac43922d132c613550f5227312637
Monique Emídio de Oliveira <49343419+moniquemidio@users.noreply.github.com>
2024-08-06 16:17:04 -0300

    Create foreach.php

---
+++ foreach.php
@@ -0,0 +1,14 @@
+<?php
+
+$itens= array('sofá', 'mesa', 'guarda-roupa', 'pia', 'fogão', 'geladeira');
+
+
+foreach ($itens as $item) {
+	//echo $item;
+	
+	if($item=='fogão') {
+		echo 'Compre um fogão e ganhe uma panela de pressão clock';
+	}
+}
+
+?>
